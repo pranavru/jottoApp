8ebc3dfb731656d4a8a5f9b730ba48c0bda6f471
Pranav Ruparelia <pranavruparelia9@gmail.com>
2021-01-12 14:17:17 -0500

    Added guessed words tests structure

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
 const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
+};
+/**
+ * Returns Redux Thunk function that dispatched GUESS_WORD action
+ * It also launches CORRECT_GUESS action conditionally, if the secret word is guessed
+ * @function guessWord
+ * @param  {string} guessedWord
+ * @returns {function} - Redux Thunk dispatcher function.
+ */
+export const guessWord = (guessedWord) => {
+  return (dispatch, getState) => {
+
+  };
 };
 
 /**
---
+++ src/reducers/guessedWordReducer.js
@@ -0,0 +1,7 @@
+/**
+ * @function successReducer
+ * @param  {boolean} state - current success state. if undefined then default is set to false.
+ * @param  {object} action - Action to be reduced.
+ * @returns {boolean} - New success state.
+ */
+export default (state = false, action) => null;
