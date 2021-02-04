c9741264b0c8dcf3e30a319b95db5143955af9a3
Pranav Ruparelia <pranavruparelia9@gmail.com>
2021-02-04 00:23:00 -0500

    Added homepage URL

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,8 @@
  * @param  {} guessedWrd
  * @param  {} secretWord
  */
-const getLetterMatchCount = (guessedWord, secretWord) => {
+// eslint-disable-next-line import/prefer-default-export
+export const getLetterMatchCount = (guessedWord, secretWord) => {
   const secretLetterSet = new Set(secretWord.split(''));
   const guessedLetterSet = new Set(guessedWord.split(''));
   return [...secretLetterSet].filter((letter) => guessedLetterSet.has(letter)).length;
